dd6d8f0ca181bde7254f7c864685506a4fa1fd9e
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-11-26 22:43:26 -0300

    api

--- api/src/useCases/orders/createOrder.ts
+++ api/src/useCases/orders/createOrder.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from 'express';
+import { io } from '../..';
 import { Order } from '../../app/models/Order';
 
 export async function createOrder(request: Request, response: Response) {
   try {
     const { table, products } = request.body;
 
-    const order = await Order.create({
-      table,
-      products,
-    });
+    const order = await Order.create({ table, products });
+    const orderDetails = await order.populate('products.product');
 
+    io.emit('orders@new', orderDetails);
     return response.status(201).json(order);
   } catch(error) {
     console.log(error);
